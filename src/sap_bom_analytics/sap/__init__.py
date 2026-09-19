0a7590116e8b5b1236d830639a3f887d27b4c93d
Diogo Ribeiro <diogo_dj@hotmail.com>
2026-09-19 10:08:24 +0100

    feat: add SAP package

---
+++ src/sap_bom_analytics/sap/__init__.py
@@ -0,0 +1,5 @@
+"""SAP extract contracts and ingestion utilities."""
+
+from sap_bom_analytics.sap.contracts import SAP_CONTRACTS, SapExtractContract
+
+__all__ = ["SAP_CONTRACTS", "SapExtractContract"]
